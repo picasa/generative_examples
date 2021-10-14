de331f4d67c15c565cdecfd1de6d06e4d3d6fa74
Pierre Casadebaig <pierre.casa@gmail.com>
2021-10-14 22:22:52 +0200

    update asemic series

--- R/functions_splines.r
+++ R/functions_splines.r
@@ -1,46 +1,17 @@
-# draw n points uniformly in in x and y
-layout_square <- function(n = 7, xlim = c(-1,1), ylim = c(-1,1)) {
-  tibble(
-    n=1:n,
-    x=runif(n, xlim[1], xlim[2]),
-    y=runif(n, ylim[1], ylim[2])
-  ) 
-}
 
-
-# draw n points in a unit circle 
-# https://mathworld.wolfram.com/DiskPointPicking.html
-layout_ellipse <- function(n = 7, r = 1, a = -pi/6) {
-  r = sqrt(runif(n, 0, r))
-  theta = runif(n, 0, 2*pi)
-  
-  # scale and rotate layout
-  layout <- tibble(
-    n = 1:n,
-    x0 = r * cos(theta),
-    y0 = r * sin(theta)) %>% 
-    mutate(x0 = x0 * 0.5) %>% 
-    mutate(
-      x = x0*cos(a) - y0*sin(a),
-      y = x0*sin(a) + y0*cos(a)) %>% 
-    select(n,x,y)
-  
-  return(layout)
-}
-
-# merge character layouts into words
+# merge character layouts into words, drop failed mappings
 layout_word <- function(word, data, shift = 1) {
   
   tibble(character = str_split(word, "")[[1]]) %>% 
-    left_join(data, by = "character") %>% 
-    mutate(d = seq(0, by = shift, length = str_length(word))) %>% 
+    left_join(data, by = "character") %>% drop_na() %>% 
+    mutate(d = seq(0, by = shift, length = n())) %>% 
     mutate(layout = map2(layout, d, ~ mutate(..1, x = x + ..2))) %>% 
     unnest(layout)
 }
 
 
 # define a character map and randomly modify few characters in y scaling
-gen_charmap <- function(n = 26, n_control = 4, n_tall = 4, size_tall = 4) {
+gen_charmap <- function(n = 26, n_control = 4, n_tall = 4, size_tall = 4, ...) {
   
   data_map <- tibble(
     pattern = 1:(n + 3),
@@ -57,21 +28,36 @@ gen_charmap <- function(n = 26, n_control = 4, n_tall = 4, size_tall = 4) {
 
 # draw a spline curve directly on layout points
 render_spline <- function(
-  data, type = "clamped", n = 100, 
+  data, type = "clamped", n = 100, orientation = "h",
   width = 0.5, alpha = 1, coord = NULL) {
   
-  data %>% 
-    ggplot(aes(x, y)) +
-    geom_bspline(
-      lineend = "round", type = type, n = n, 
-      size = width, alpha = alpha) +
-    coord + theme_void()
+  switch(
+    orientation,
+    
+    h = {
+      plot <- data %>% 
+        ggplot(aes(x, y)) +
+        geom_bspline(
+          lineend = "round", type = type, n = n, size = width, alpha = alpha) 
+    }, 
+    
+    v = {
+      plot <- data %>% 
+        ggplot(aes(x = y, y = -x)) +
+        geom_bspline(
+          lineend = "round", type = type, n = n, size = width, alpha = alpha) 
+    }
+  )
+  
+  return(plot + coord + theme_void())
   
 }
 
 
-# render a paragraph from an existing text and a set of characters
-render_script <- function(text, data, ncol = 80, scale = 0.9) {
+# render a paragraph from an existing text and a set of plots using script style
+render_script <- function(
+  text, data, ncol = 80, scale = 0.9,
+  orientation = "h", align = "none") {
   # split text to characters
   seq <- text %>% str_to_lower() %>% str_split(., "") 
   
@@ -83,12 +69,27 @@ render_script <- function(text, data, ncol = 80, scale = 0.9) {
     group_by(position, character) %>% 
     slice_sample(n = 1)
   
-  plot_grid(plotlist = glyph$plot, ncol=ncol, scale=scale)
+  # render the character string
+  switch (
+    orientation,
+    
+    h = {
+      plot_grid(
+        plotlist = glyph$plot, ncol = ncol, scale = scale, align = align)
+      },
+    
+    v = {
+      plot_grid(
+        plotlist = glyph$plot, nrow = n_col,
+        scale = scale, align = align, byrow = FALSE)
+      }
+  )
+  
 }
 
 
 # render a line of glyphs - words
-render_line <- function(data, ncol = 80,  scale = 0.9) {
+render_line <- function(data, ncol = 80, scale = 0.9) {
   plot_grid(
     plotlist = c(data$plot, list(NULL)),
     rel_widths = c(data$length, ncol - sum(data$length)),
